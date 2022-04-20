610f702474e899e5b6cd207835efed992846c1e2
Lakshman31 <103436599+Lakshman31@users.noreply.github.com>
2022-04-20 18:51:26 +0530

    Update LinePlot-Covidcases.py

--- LinePlot-Covidcases.py
+++ LinePlot-Covidcases.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df1 = pd.read_csv('C:/Users/laksh/Downloads/covidcases.csv')
-df = df1.dropna()
-print(df.isnull().sum())
-plt.plot(df['category'],df['Actual Data'],'b',label='Actual data')
-plt.plot(df['category'],df['Model Computed Data'],'g',label='Predicted data')
+
+data_orginal = pd.read_csv('C:/Users/laksh/Downloads/covidcases.csv')
+data = data_orginal.dropna()
+print(data.isnull().sum())
+plt.plot(data['category'],data['Actual Data'],'b',label='Actual data')
+plt.plot(data['category'],data['Model Computed Data'],'g',label='Predicted data')
 plt.xlabel("Date")
 plt.ylabel("Covid cases")
 plt.legend()
