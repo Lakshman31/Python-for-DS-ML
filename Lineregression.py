cefec2e47984e0921d6b0d381ce5fb5be93983d8
Lakshman31 <103436599+Lakshman31@users.noreply.github.com>
2022-04-20 18:34:08 +0530

    Update Lineregression.py

--- Lineregression.py
+++ Lineregression.py
@@ -2,13 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 import numpy as np
-df1 = pd.read_csv('covidcases.csv',parse_dates='True')
-df = df1.dropna()
-app = FastAPI()
-#df['category']=pd.to_datetime(df['category'])
-X = np.array(df['Actual Data'].values).reshape(-1,1)
-Y = np.array(df.index.values).reshape(-1,1)
-W = np.array(df['Model Computed Data'].values).reshape(-1,1)
+
+data_orginal = pd.read_csv('covidcases.csv',parse_dates='True')
+data = data_org.dropna()
+X = np.array(data['Actual Data'].values).reshape(-1,1)
+Y = np.array(data.index.values).reshape(-1,1)
 reg=linear_model.LinearRegression()
 reg.fit(X,Y)
 val = np.array(float(input("enter the day"))).reshape(-1,1)
